29c0a9da283ad234f8242eaca37a4a4a4273fc4a
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-04-25 09:20:19 +0300

    end task 01-07

--- JS/task-06.js
+++ JS/task-06.js
@@ -1,27 +1,21 @@
-// Напиши скрипт, который бы при потере фокуса на инпуте,
-// проверял его содержимое на правильное количество символов.
-// Сколько символов должно быть в инпуте,
-//     указывается в его атрибуте data - length.
-// Если введено подходящее количество, то border инпута становится зеленым,
-//     если неправильное - красным.
-
-
 const input = document.getElementById("validation-input")
 
-const validLength = input.dataset.length
+// const validLength = input.dataset.length
 
 const validInputLength = function (event) {   
-    if (event.currentTarget.value.length === validLength) {
-      
-       return input.classList.add('valid')
+    if (event.currentTarget.value.length === 6) {      
+        input.classList.add('valid')
+        // input.classList.remove('invalid')
     }
-    else {
-        // return input.classList.toggle('invalid')
+    if (event.currentTarget.value.length !== 6) {
+         input.classList.add('invalid')
+        // input.classList.remove('valid')
+    }
+     if (event.currentTarget.value.length === 0) {
+         input.classList.remove('invalid')
+        input.classList.remove('valid')
     }
 }
 //  console.log(input.currentTarget.value.length)
 
-
-
 input.addEventListener('blur', validInputLength)
-
